22f917f3224309acc5445614342e73a325d6b549
Ilana Kantorov <ilanak@microsoft.com>
2018-02-28 15:21:28 +0000

    Upgrade Azure monitor api to 2017-05-01-preview
    * Delete unsupported paramater $orderby
    * Fix exception loggin

--- test/plugin/test_in_azuremonitormetrics.rb
+++ test/plugin/test_in_azuremonitormetrics.rb
@@ -13,14 +13,14 @@ class AzureMonitorMetricsInputTest < Test::Unit::TestCase
     client_secret test_client_secret
 
     timespan          300
-    aggregation       Average,count
     interval          PT1M
     resource_uri      /subscriptions/b324c52b-4073-4807-93af-e07d289c093e/resourceGroups/test/providers/Microsoft.Storage/storageAccounts/larryshoebox/blobServices/default/providers/Microsoft.Insights/metrics/BlobCapacity
+    aggregation       Average,count
     top               20
-    orderby           sum asc
-    filter            timeGrain eq duration'PT1M' and (name.value eq 'Network Out' or name.value eq 'Percentage CPU') and (aggregationType eq 'Average' or aggregationType eq 'Count') and startTime eq 2017-10-11T23:00:00Z and endTime eq 2017-11-22T15:00:00Z
+    filter            A eq 'a1' and B eq '*'
     result_type       Success
-    api_version       2016-09-01
+    metrics           Percentage CPU
+    api_version       2017-05-01-preview
   ]
 
   def create_driver_monitor_metrics(conf = CONFIG_MONITOR_METRICS)
@@ -33,12 +33,14 @@ class AzureMonitorMetricsInputTest < Test::Unit::TestCase
     assert_equal 'test_tenant_id', d.instance.tenant_id
     assert_equal 'test_client_id', d.instance.client_id
     assert_equal 300, d.instance.timespan
+    assert_equal 'PT1M', d.instance.interval
     assert_equal '/subscriptions/b324c52b-4073-4807-93af-e07d289c093e/resourceGroups/test/providers/Microsoft.Storage/storageAccounts/larryshoebox/blobServices/default/providers/Microsoft.Insights/metrics/BlobCapacity', d.instance.resource_uri
+    assert_equal 'Average,count', d.instance.aggregation
     assert_equal 20, d.instance.top
-    assert_equal 'sum asc', d.instance.orderby
-    assert_equal "timeGrain eq duration'PT1M' and (name.value eq 'Network Out' or name.value eq 'Percentage CPU') and (aggregationType eq 'Average' or aggregationType eq 'Count') and startTime eq 2017-10-11T23:00:00Z and endTime eq 2017-11-22T15:00:00Z", d.instance.filter
+    assert_equal 'A eq \'a1\' and B eq \'*\'', d.instance.filter
     assert_equal 'Success', d.instance.result_type
-    assert_equal '2016-09-01', d.instance.api_version
+    assert_equal 'Percentage CPU', d.instance.metrics
+    assert_equal '2017-05-01-preview', d.instance.api_version
   end
 
   def test_set_query_options
@@ -46,11 +48,13 @@ class AzureMonitorMetricsInputTest < Test::Unit::TestCase
     start_time = Time.now - 1000
     end_time = Time.now
     query_options = d.instance.set_path_options(start_time, end_time, {})
-    assert_equal '2016-09-01', query_options[:query_params]['api-version']
-    assert_equal "timeGrain eq duration'PT1M' and (name.value eq 'Network Out' or name.value eq 'Percentage CPU') and (aggregationType eq 'Average' or aggregationType eq 'Count') and startTime eq #{start_time.utc.iso8601} and endTime eq #{end_time.utc.iso8601}", query_options[:query_params]['$filter']
+    assert_equal '2017-05-01-preview', query_options[:query_params]['api-version']
+    assert_equal 'A eq \'a1\' and B eq \'*\'', query_options[:query_params]['$filter']
+    assert_equal 'Average,count', query_options[:query_params]['aggregation']
+    assert_equal 'PT1M', query_options[:query_params]['interval']
     assert_equal 20, query_options[:query_params]['$top']
-    assert_equal 'sum asc', query_options[:query_params]['$orderby']
     assert_equal 'Success', query_options[:query_params]['resultType']
+    assert_equal 'Percentage CPU', query_options[:query_params]['metric']
     assert_equal '/subscriptions/b324c52b-4073-4807-93af-e07d289c093e/resourceGroups/test/providers/Microsoft.Storage/storageAccounts/larryshoebox/blobServices/default/providers/Microsoft.Insights/metrics/BlobCapacity', query_options[:path_params]['resourceUri']
   end
 
